6594bf6cad6c24fd9191289021f89228bfc12423
powerless <asphalt1018@gmail.com>
2023-04-20 10:22:28 +0800

    add fn

--- src/vm.rs
+++ src/vm.rs
@@ -1,17 +1,69 @@
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::io::Seek;
+use std::ops::Mul;
 use crate::bytecode::ByteCode;
 use crate::value::Value;
 use crate::parse::ParseProto;
 
 // It supports only 1 argument and assumes the argument is at index:1 on stack.
-fn lib_print(state:&mut ExeState) -> i32{
-    println!("{:?}",state.stack([1]));
+fn lib_print(state: &mut ExeState) -> i32 {
+    println!("{:?}", state.stack([1]));
     0
 }
 
-pub struct ExeState{
-    globals:HashMap<String,Value>,
-    stack:Vec<Value>,
+pub struct ExeState {
+    globals: HashMap<String, Value>,
+    stack: Vec<Value>,
+}
+
+impl ExeState {
+    // new fn
+    pub fn new() -> Self {
+        let mut globals = HashMap::new();
+        globals.insert(String::from("print"), Value::Function(lib_print));
+        ExeState {
+            globals,
+            stack: Vec::new(),
+        }
+    }
+
+    // execute fn
+    pub fn execute(&mut self, proto: &ParseProto) {
+        for code in proto.byte_codes.iter() {
+            match *code {
+                ByteCode::GetGlobal(dst, name) => {
+                    let name = &proto.constants[name as usize];
+                    if let Value::ToString(key) = name {
+                        let v = self.globals.get(key).unwrap_or(&Value::Nil).clone();
+                        self.set_stack(dst, v);
+                    } else {
+                        panic!("invalid global key: {name:?}")
+                    }
+                }
+                ByteCode::LoadConst(dst, c) => {
+                    let v = proto.constants[c as usize].clone();
+                    self.set_stack(dst, v);
+                }
+                ByteCode::Call(func, _) => {
+                    let func = &self.stack[func as usize];
+                    if let Value::Function(f) = func {
+                        f(self);
+                    } else {
+                        panic!("invalid function: {func:?}")
+                    }
+                }
+            }
+        }
+    }
+
+    // set_stack fn
+    fn set_stack(&mut self, dst: u8, v: Value) {
+        let dst = dst as usize;
+        match dst.cmp(&self.stack.len()) {
+            Ordering::Equal => self.stack.push(v),
+            Ordering::Less => self.stack[dst] = v,
+            Ordering::Greater => panic!("fail in set_stack"),
+        }
+    }
 }
