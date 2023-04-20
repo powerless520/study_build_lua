92e4f3a2b4419ac5ccff50892b78fb45877624a3
powerless <asphalt1018@gmail.com>
2023-04-20 16:13:17 +0800

    day2

--- src/lex.rs
+++ src/lex.rs
@@ -1,66 +1,309 @@
+use std::ffi::c_long;
 use std::fs::File;
 use std::io::{Read, Seek, SeekFrom};
+use std::num::TryFromIntError;
+use crate::lex::Token::{Else, Len, Less, Repeat};
 
 #[derive(Debug)]
 pub enum Token {
-    Name(String),
+    // keywords
+    And,
+    Break,
+    Do,
+    Else,
+    Elseif,
+    End,
+    False,
+    For,
+    Function,
+    Goto,
+    If,
+    In,
+    Local,
+    Nil,
+    Not,
+    Or,
+    Repeat,
+    Return,
+    Then,
+    True,
+    Until,
+    While,
+
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Pow,
+    Len,
+    BitAnd,
+    BixXor,
+    BitOr,
+    ShiftL,
+    ShiftR,
+    Idiv,
+    Equal,
+    NotEq,
+    LesEq,
+    GreEq,
+    Less,
+    Greater,
+    Assign,
+    ParL,
+    ParR,
+    CurlyL,
+    CurlyR,
+    SqurL,
+    SqurR,
+    DoubColon,
+    SemiColon,
+    Colon,
+    Comma,
+    Dot,
+    Concat,
+    Dots,
+
+    // constant values
+    ToInteger(i64),
+    ToFloat(f64),
+
     ToString(String),
+
+    Name(String),
+
     Eos,
 }
 
 #[derive(Debug)]
 pub struct Lex {
     input: File,
+    ahead: Token::Eos,
 }
 
 impl Lex {
     pub fn new(input: File) -> Lex {
         Lex {
             input,
+            ahead: Token::Eos,
         }
     }
     pub fn next(&mut self) -> Token {
         let ch = self::read_char(self);
         match ch {
             ' ' | '\r' | '\n' | '\t' => self::next(),
+            '+' => Token::Add,
+            '*' => Token::Mul,
+            '%' => Token::Mod,
+            '^' => Token::Pow,
+            '#' => Token::Len,
+            '&' => Token::BitAnd,
+            '|' => Token::BitOr,
+            '(' => Token::ParL,
+            ')' => Token::ParR,
+            '{' => Token::CurlyL,
+            '}' => Token::CurlyR,
+            '[' => Token::SqurL,
+            ']' => Token::SqurR,
+            ';' => Token::SemiColon,
+            ',' => Token::Comma,
+            '/' => self.check_ahead('/', Token::Idiv, Token::Div),
+            '=' => self.check_ahead('=', Token::Equal, Token::Assign),
+            '~' => self.check_ahead('=', Token::NotEq, Token::BitXor),
+            ':' => self.check_ahead(':', Token::DoubColon, Token::Colon),
+            '<' => self.check_ahead2('=', Token::LesEq, '<', Token::ShiftL, Token::Less),
+            '>' => self.check_ahead2('=', Token::GreEq, '>', Token::ShiftR, Token::Greater),
             '\0' => Token::Eos,
-            '"' => {
-                let mut s = String::new();
-                loop {
-                    match self.read_char() {
-                        '\0' => panic!("unfinsihed literal string"),
-                        '"' => break,
-                        ch => s.push(ch)
+            '\'' | '"' => self.read_string(ch),
+            '.' => match self.read_char() {
+                '.' => {
+                    if self.read_char() == '.' {
+                        Token::Dots
+                    } else {
+                        self.putback_char();
+                        Token::Concat
                     }
                 }
-                Token::ToString(s)
-            }
-            'A'..='Z' | 'a'..='z' | '_' => {
-                let mut name = String::new();
-                name.push(ch);
-                loop {
-                    match self.read_char() {
-                        '\0' => break,
-                        '_' => name.push('_'),
-                        ch if ch.is_alphanumeric() => name.push(ch),
-                        _ => {
-                            self.input.seek(SeekFrom::Current(-1)).unwrap();
-                            break;
-                        }
-                    }
+                '0'..='9' => {
+                    self.putback_char();
+                    self.read_number_fraction(0)
                 }
-                Token::Name(name)
+                _ => {
+                    self.putback_char();
+                    Token::Dot
+                }
+            },
+            '-' => {
+                if self.read_char() == '-' {
+                    self.read_comment();
+                    self.do_next()
+                } else {
+                    self.putback_char();
+                    Token::Sub
+                }
             }
-            _ => panic!("unexpeceted char:{}", ch)
+            '0'..='9' => self.read_number(ch),
+            'A'..='Z' | 'a'..='z' | '_' => self.read_name(ch),
+            '\0' => Token::Eos,
+            _ => panic!("invalid char {ch}"),
         }
     }
 
+    #[allow(clippy::unused_io_amount)]
     fn read_char(&mut self) -> char {
-        let mut buf: [u8; 1] = [0; 1];
-        if self.input.read(&mut buf).unwrap() == 1 {
-            buf[0] as char
+        let mut buf: [u8; 1] = [0];
+        self.input.read(&mut buf).unwrap();
+        buf[0] as char
+    }
+
+    fn putback_char(&mut self) {
+        self.input.seek(SeekFrom::Current(-1)).unwrap();
+    }
+
+    fn check_ahead(&mut self, ahead: char, long: Token, short: Token) -> Token {
+        if self.read_char() == ahead {
+            long
         } else {
-            '\0'
+            self.putback_char();
+            short
+        }
+    }
+
+    fn check_ahead2(&mut self, ahead1: char, long1: Token, ahead2: Token, long2: Token, short: Token) -> {
+        let ch = self.read_char();
+        if ch == ahead1 {
+            long1
+        } else if ch == ahead2 {
+            long2
+        } else {
+            self.putback_char();
+            short
+        }
+    }
+
+    fn read_number(&mut self, first: char) -> Token {
+        if first == '0' {
+            let second = self.read_char();
+            if second == 'x' || second == 'X' {
+                return self.read_heximail();
+            }
+            self.putback_char();
+        }
+
+        // decimal
+        let mut n = char::to_digit(first, 10).unwrap() as i64;
+        loop {
+            let ch = self.read_char();
+            if let Some(d) = char::to_digit(ch, 10) {
+                n = n * 10 + d as i64;
+            } else if ch == '.' {
+                return self.read_number_fraction(n);
+            } else if ch == 'e' || ch == 'E' {
+                return self.read_number_exp(n as i64);
+            } else {
+                self.putback_char();
+                break;
+            }
+        }
+
+        // check following
+        let fch = self.read_char();
+        if fch.is_alphanumeric() || fch == '.' {
+            panic!("malformat number");
+        } else {
+            self.putback_char();
+        }
+        Token::ToInteger(n)
+    }
+
+    fn read_number_fraction(&mut self, i: i64) -> Token {
+        let mut n: i64 = 0;
+        let mut x: f64 = 1.0;
+
+        loop {
+            let ch = self.read_char();
+            if let Some(d) = char::to_digit(ch, 10) {
+                n = n * 10 + d as i64;
+                x *= 10.0;
+            } else {
+                self.putback_char();
+                break;
+            }
+        }
+        Token::ToFloat(i as f64 + n as f64 / x)
+    }
+
+    fn read_number_exp(&mut self, _: f64) -> Token {
+        todo!("lex number exp")
+    }
+
+    fn read_heximal(&mut self) -> Token {
+        todo!("lex heximal")
+    }
+
+    fn read_string(&mut self, quote: char) -> Token {
+        let mut s = String::new();
+        loop {
+            match self.read_char() {
+                '\n' | '\0' => panic!("unfinished string"),
+                '\\' => todo!("escape"),
+                ch if ch == quote => break,
+                ch => s.push(ch),
+            }
+        }
+        Token::ToString(s)
+    }
+
+    fn read_name(&mut self, first: char) -> Token {
+        let mut s = first.to_string();
+        loop {
+            let ch = self.read_char();
+            if ch.is_alphanumeric() || ch == '_' {
+                s.push(ch);
+            } else {
+                self.putback_char();
+                break;
+            }
+        }
+
+        match &s as &str {
+            "and" => Token::Add,
+            "break" => Token::Break,
+            "do" => Token::Do,
+            "else" => Token::Else,
+            "elseif" => Token::Elseif,
+            "end" => Token::End,
+            "false" => Token::False,
+            "for" => Token::For,
+            "function" => Token::Function,
+            "goto" => Token::Goto,
+            "if" => Token::If,
+            "in" => Token::In,
+            "local" => Token::Local,
+            "nil" => Token::Nil,
+            "not" => Token::Not,
+            "or" => Token::Or,
+            "repeat" => Token::Repeat,
+            "return" => Token::Return,
+            "then" => Token::Then,
+            "true" => Token::True,
+            "until" => Token::Until,
+            "while" => Token::While,
+            _ => Token::Name(s),
+        }
+    }
+
+    fn read_comment(&mut self) {
+        match self.read_char() {
+            '[' => todo!("long comment"),
+            _ => {
+                loop {
+                    let ch = self.read_char();
+                    if ch == '\n' || ch == '\0' {
+                        break;
+                    }
+                }
+            }
         }
     }
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,38 +1,172 @@
+use std::any::Any;
 use std::fs::File;
+use std::num::NonZeroI16;
 use crate::lex::{Lex, Token};
 use crate::bytecode::ByteCode;
+use crate::lex::Token::{BitAnd, Less, Name, True};
 use crate::value::Value;
 
 #[derive ! (Debug)]
 pub struct ParseProto {
     pub constants: Vec<Value>,
     pub byte_codes: Vec<ByteCode>,
+
+    locals: Vec<String>,
+    lex: Lex,
 }
 
-pub fn load(input: File) -> ParseProto {
-    let mut constants = Vec::new();
-    let mut byte_codes = Vec::new();
-    let mut lex = Lex::new(input);
+impl ParseProto {
+    pub fn load(input: File) -> ParseProto {
+        let mut proto = ParseProto {
+            constants: Vec::new(),
+            byte_codes: Vec::new(),
+            locals: Vec::new(),
+            lex: Lex::new(input),
+        };
 
-    loop {
-        match lex.next() {
-            Token::Name(name) => {
-                constants.push(Value::ToString(name));
-                byte_codes.push(ByteCode::LoadConst(0, (constants.len() - 1) as u8));
-                if let Token::ToString(s) = lex.next() {
-                    constants.push(Value::ToString(s));
-                    byte_codes.push(ByteCode::LoadConst(1, (constants.len() - 1) as u8));
-                    byte_codes.push(ByteCode::Call(0, 1));
-                } else {
-                    panic!("expected string")
+        proto.chunk();
+
+        println!("constants: {:?}", &proto.constants);
+        println!("byte_codes");
+        for c in proto.byte_codes.iter() {
+            println!("{:?}", c);
+        }
+        proto
+    }
+
+    fn chunk(&mut self) {
+        loop {
+            match self.lex.next() {
+                Token::Name(name) => {
+                    if self.lex.peek() == &Token::Assign {
+                        self.assignment(name);
+                    } else {
+                        self.function_call(name)
+                    }
                 }
+                Token::Local => self.local(),
+                Token::Eos => break,
+                t => panic!("unexpected token: {t:?}"),
             }
-            Token::Eos => break,
-            t => panic!("unexpected token: {t:?}"),
         }
     }
 
-    dbg!(&constants);
-    dbg!(&byte_codes);
-    ParseProto { constants, byte_codes }
+    // Name LiteralString
+    // Name (exp)
+    fn function_call(&mut self, name: String) {
+        let ifunc = self.locals.len();
+        let iarg = ifunc + 1;
+
+        // function,variable
+        let code = self.load_var(ifunc, name);
+        self.byte_codes.push(code);
+
+        // argument, (exp) or "string"
+        match self.lex.next() {
+            Token::ParL => {
+                self.load_exp(iarg);
+
+                if self.lex.next() != Token::ParR {
+                    panic!("expected `)`");
+                }
+            }
+            Token::ToString(s) => {
+                let code = self.load_const(iarg, Value::ToString(s));
+                self.byte_codes.push(code);
+            }
+            _ => panic!("expected string"),
+        }
+        self.byte_codes.push(ByteCode::Call(ifunc as u8, 1));
+    }
+
+    // local Name = exp
+    fn local(&mut self) {
+        let var = if let Token::Name(var) = self.lex.next() {
+            var
+        } else {
+            panic!("expected variable");
+        };
+        if self.lex.next() != Token::Assign {
+            panic!("expected `=`");
+        }
+
+        self.load_exp(self.locals.len());
+
+        // add to locals after load_exp()
+        self.locals.push(var);
+    }
+
+    fn assignment(&mut self, var: String) {
+        self.lex.next();
+
+        if let Some(i) = self.get_local(&var) {
+            self.load_exp(i)
+        } else {
+            let dst = self.add_const(Value::ToString(var)) as u8;
+
+            let code = match self.lex.next() {
+                Token::Nil => ByteCode::SetGlobalConst(dst, self.add_const(Value::Nil) as u8),
+                Token::True => ByteCode::SetGlobalConst(dst, self.add_const(Value::Boolean(True)) as u8),
+                Token::False => ByteCode::SetGlobalConst(dst, self.add_const(Value::Boolean(True)) as u8),
+                Token::ToInteger(i) => ByteCode::SetGlobalConst(dst, self.add_const(Value::Integer(i)) as u8),
+                Token::ToFloat(f) => ByteCode::SetGlobalConst(dst, self.add_const(Value::Float(f)) as u8),
+                Token::ToString(s) => ByteCode::SetGlobalConst(dst, self.add_const(Value::String(s)) as u8),
+
+                // from variable
+                Token::Name(var) => {
+                    if let Some(i) = self.get_local(&var) {
+                        ByteCode::SetGlobal(dst, i as u8)
+                    } else {
+                        ByteCode::SetGlobalGlobal(dst, self.add_const(Value::ToString(var)) as u8)
+                    }
+                }
+                _ => panic!(invalid argument)
+            };
+            self.byte_codes.push(code)
+        }
+    }
+
+    fn add_const(&mut self, c: Value) -> usize {
+        let constants = &mut self.constants;
+        constants.iter().position(|v| v == &c).unwrap_or_else(|| {
+            constants.push(c);
+            constants.len() - 1
+        })
+    }
+
+    fn load_const(&mut self,dst:usize,c:Value) -> ByteCode{
+        ByteCode::LoadConst(dst as u8,self.add_const(c) as u16)
+    }
+
+    fn load_var(&mut self, dst: usize, name: String) -> ByteCode {
+        if let Some(i) = self.get_local(&name) {
+            ByteCode::Move(dst as u8, i as u8)
+        } else {
+            let ic = self.add_const(Value::ToString(name));
+            ByteCode::GetGlobal(dst as u8, ic as u8)
+        }
+    }
+
+    fn get_local(&self, name: &str) -> Option<usize> {
+        self.locals.iter().rposition(|v| v == name)
+    }
+
+    fn load_exp(&mut self, dst: usize) {
+        let code = match self.lex.next() {
+            Token::Nil => ByteCode::LoadNil(dst as u8),
+            Token::True => ByteCode::LoadBool(dst as u8, true),
+            Token::False => ByteCode::LoadBool(dst as u8, false),
+            Token::Integer(i) =>
+                if let Ok(ii) = i16::try_from(i) {
+                    ByteCode::LoadInt(dst as u8, ii)
+                } else {
+                    self.load_const(dst, Value::Integer(i))
+                }
+            Token::Float(f) => self.load_const(dst, Value::Float(f)),
+            Token::String(s) => self.load_const(dst, Value::String(s)),
+            Token::Name(var) => self.load_var(dst, var),
+            _ => panic!("invalid argument"),
+        };
+        self.byte_codes.push(code);
+    }
 }
