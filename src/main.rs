3b3b2fd52512aeaf4cafc47c752253a6c0836594
powerless <asphalt1018@gmail.com>
2023-04-24 11:16:44 +0800

    day5

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-mod lex;
-mod parse;
-mod value;
-mod bytecode;
-mod vm;
+use std::cmp::Ordering;
+use crate::value::Value;
 
-fn main() {
-    println!("Hello, world!");
+pub fn ftoi(f: f64) -> Option<i64> {
+    let i = f as i64;
+    if i as f64 != f {
+        None
+    } else {
+        Some(i)
+    }
+}
+
+pub fn set_vec(vec: &mut Vec<Value>, u: usize, value: Value) {
+    match i.cmp(&vec.len()) {
+        Ordering::Less => vec[i] = value,
+        Ordering::Equal => vec.push(value),
+        Ordering::Greater => {
+            vec.resize(i, Value::Nil);
+            vec.push(value);
+        }
+    }
 }
